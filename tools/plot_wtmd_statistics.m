1f75c21c25ac7323b653f0f8edce02ee1e996d2d
yongdd <yongdd@kias.re.kr>
2023-09-25 17:44:29 +0900

    adding a plot in matlab tool

--- tools/plot_wtmd_statistics.m
+++ tools/plot_wtmd_statistics.m
@@ -2,24 +2,36 @@ clear all;
 
 load("wtmd_statistics_1000000.mat");
 
-coeff_t = 1/dT+1;
-exp_u = exp(u*coeff_t);
-y = exp_u/sum(exp_u)/dpsi;
-
-% Plot U(Psi)
+% Plot U(Ψ)
 figure(1);
 plot(psi_range, u);
 xlabel('\Psi')
 ylabel('U(\Psi)')
 
-% Plot U'(Psi)
+% Plot U'(Ψ)
 figure(2);
 plot(psi_range, up);
 xlabel('\psi')
 ylabel('U^\prime(\Psi)')
 
-% Plot P(Psi)
+% Plot P(Ψ)
 figure(3);
+
+coeff_t = 1/dT+1;
+exp_u = exp(u*coeff_t);
+y = exp_u/sum(exp_u)/dpsi;
+
 plot(psi_range, y);
 xlabel('\Psi')
 ylabel('P(\Psi)')
+
+% Plot P(Ψ)
+figure(4);
+
+cutoff = 1e-9;
+x = psi_range(I0 > cutoff);
+y = dH_psi_A_B(I0 > cutoff);
+
+plot(x, y);
+xlabel('\Psi')
+ylabel('\partial F/\partial{\it \chi_{b, AB} N}')
